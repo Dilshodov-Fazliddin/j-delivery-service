e7f10e6cb3dc4382d43a8d0c831a3c5da96dcccb
Fazliddin <dilshodovfazliddin@gmail.com>
2026-02-17 12:54:47 +0500

    NotificationAdapter fix

--- src/main/java/uzumtech/jdeliveryservice/component/NotificationAdapter.java
+++ src/main/java/uzumtech/jdeliveryservice/component/NotificationAdapter.java
@@ -9,35 +9,37 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import uzumtech.jdeliveryservice.dto.request.NotificationSmsRequest;
 import uzumtech.jdeliveryservice.dto.request.ParcelRequest;
+import uzumtech.jdeliveryservice.dto.request.Receiver;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE)
 public class NotificationAdapter {
+
     final RestClient restClient;
 
     @Value("${notification.service.merchantLogin}")
     String merchantLogin;
     @Value("${notification.service.merchantPassword}")
     String merchantPassword;
-    @Value("${notification.service.id}")
-    String id;
     @Value("${notification.service.url.mail}")
     String url;
 
 
     public void sendNotification(String message,String email) {
         var result = restClient.post()
-                .uri(url + id )
+                .uri(url)
                 .headers(headers->{
                     headers.setBasicAuth(merchantLogin,merchantPassword);
                     headers.setContentType(MediaType.APPLICATION_JSON);
                 })
-                .body(new NotificationSmsRequest(message,email))
+                .body(new NotificationSmsRequest(new Receiver(email),"EMAIL",message))
                 .retrieve()
                 .toBodilessEntity();
 
         log.info("{}", result);
+
     }
+
 }
---
+++ src/main/java/uzumtech/jdeliveryservice/dto/request/Receiver.java
@@ -0,0 +1,4 @@
+package uzumtech.jdeliveryservice.dto.request;
+
+public record Receiver(String email) {
+}
